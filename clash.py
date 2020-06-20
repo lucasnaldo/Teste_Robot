0125027d44fcd8a5846918b6db6a3d119ed5dce0
Lucas Naldo <bi02@brdom.local>
2020-06-20 14:53:32 -0300

    ups

--- clash.py
+++ clash.py
@@ -5,78 +5,130 @@ import csv
 import pandas as pd
 import logging
 from robot.libraries.BuiltIn import BuiltIn
+from robot.api import logger
 
 
 logging.basicConfig(level=logging.DEBUG)
 logging.basicConfig(level=logging.DEBUG, filename='clash.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
-try:  
-    logging.info("Acessando os clans com nome 'The resistance'")
-    url_clans = "https://api.clashroyale.com/v1/clans/?tag=%239V2Y&name=The resistance"
-    payload = {}
-    headers = {
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijg2YWVjZDEyLWU0NTYtNGEwNS05NzNlLTgyZDcwODEyYTYwYiIsImlhdCI6MTU5MjQ1MDcyNCwic3ViIjoiZGV2ZWxvcGVyLzQ1ODU4MmVjLTNhNDgtZDUzNC1kNWZmLTdiYjBkMmI5ZTU2MiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxNzcuNDUuMzUuMjUiXSwidHlwZSI6ImNsaWVudCJ9XX0.e5FZ0AzCrW_VYVwI5D1PJCSAR6nJWU8YOdDMaL71TnJU6NkJ-JwILuSB7P5jximgxWv1nIaxi6IMgLU0weDu9w'
-    }
-    logging.warning("Utilizando o Token fixado no codigo")
-    response = requests.get(url_clans, headers=headers, data = payload)
-    data = response.text.encode('utf8')
-    j_data = json.loads(data)
-    items = j_data['items']
-    listinha = []
-    for item in items:
-        dictzinho = {}
-        if 'location' in item and item['location']:
-            loc = item['location']
-            if 'countryCode' in loc and loc['countryCode']:
-                if loc['countryCode'] == 'BR':
-                    if re.search('#9V2Y', item['tag'], re.IGNORECASE):
-                        dictzinho = item
-                        tag = item['tag']
-                        tag_corrigida = tag.replace('#', '%23')
-                        listinha.append(dictzinho)
-                        
-    
+def ip_get():
+    try:
+        ip = requests.get('https://api.ipify.org').text
+        logger.console(ip)
+        # print(ip)
         
-except Exception as ex:
-    logging.error(ex)
-    raise ex
-        
-try:
-    logging.info('Acessando os membros do clan usando a tag do clan')
-    url_clans_members = "https://api.clashroyale.com/v1/clans/{}/members".format(tag_corrigida)
-    payload = {}
-    headers = {
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijg2YWVjZDEyLWU0NTYtNGEwNS05NzNlLTgyZDcwODEyYTYwYiIsImlhdCI6MTU5MjQ1MDcyNCwic3ViIjoiZGV2ZWxvcGVyLzQ1ODU4MmVjLTNhNDgtZDUzNC1kNWZmLTdiYjBkMmI5ZTU2MiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxNzcuNDUuMzUuMjUiXSwidHlwZSI6ImNsaWVudCJ9XX0.e5FZ0AzCrW_VYVwI5D1PJCSAR6nJWU8YOdDMaL71TnJU6NkJ-JwILuSB7P5jximgxWv1nIaxi6IMgLU0weDu9w'
-    }
+        return ip
     
-    response_clans_members = requests.get(url_clans_members, headers=headers, data = payload)
-    data_clans_members = response_clans_members.text.encode('utf8')
-    j_data_clans_members = json.loads(data_clans_members)
-    items_clans_members = j_data_clans_members['items']
-    
-    member_list = []
-    for item_member in items_clans_members:
-        member = {}
-        if 'name' in item_member and item_member['name']:
-            member['nome'] = item_member['name']
-        if 'expLevel' in item_member and item_member['expLevel']:
-            member['level'] = item_member['expLevel']
-        if 'trophies' in item_member and item_member['trophies']:
-            member['troféus'] = item_member['trophies']
-        if 'role' in item_member and item_member['role']:
-            member['papel'] = item_member['role']
-            
-        member_list.append(member)
-        
-except Exception as ex:
-    logging.error(ex)
-    raise ex
+    except Exception as ex:
+        logging.error(ex)
+        raise ex
 
-try:
-    logging.info('Gerando arquivo member_list.csv')
-    df = pd.DataFrame(member_list)
-    df.to_csv('member_list.csv', index=None, encoding='utf-8-sig')
-    
-except Exception as ex:
-    logging.error(ex)
-    raise ex
+def get_clan(token):
+    try:  
+        logger.console("ENTROU NO GET CLAN")
+        logging.info("Acessando os clans com nome 'The resistance'")
+        
+        # setando parametros de requisição
+        url_clans = "https://api.clashroyale.com/v1/clans/?tag=%239V2Y&name=The resistance"
+        payload = {}
+        headers = {
+        'Authorization': 'Bearer {}'.format(str(token))
+        }
+        
+        logging.warning("Utilizando o Token fixado no codigo")
+        
+        # Efetuando chamada a api passando o nome do clan como referencia
+        response = requests.get(url_clans, headers=headers, data = payload)
+        data = response.text.encode('utf8')
+        
+        # Corvertendo retorno para json
+        j_data = json.loads(data)
+        items = j_data['items']
+        
+        # Criando nova lista
+        listinha = []
+        
+        # Percorrendo a lista do retorno para pegar apenas as informações necessárias
+        for item in items:
+            dictzinho = {}
+            if 'location' in item and item['location']:
+                loc = item['location']
+                if 'countryCode' in loc and loc['countryCode']:
+                    
+                    # Verificando clans que sejam do brasil
+                    if loc['countryCode'] == 'BR':
+                        
+                        # Procurando começo da TAG
+                        if re.search('#9V2Y', item['tag'], re.IGNORECASE):
+                            dictzinho = item
+                            tag = item['tag']
+                            
+                            # Salvando a Tag tratada em variavel
+                            tag_corrigida = tag.replace('#', '%23')
+                            listinha.append(dictzinho)
+            
+            # Retorna Tag tratada para segunda chamada              
+            print(tag_corrigida)
+            logger.console(tag_corrigida)
+
+        logger.console("ENTROU NO GET MEMBERS")
+        logging.info('Acessando os membros do clan usando a tag do clan')
+        
+        # Setando parametros para a chamada de membros do clan, passando a variavel da Tag formatada
+        url_clans_members = "https://api.clashroyale.com/v1/clans/{}/members".format(str(tag_corrigida))
+        payload = {}
+        headers = {
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijg2YWVjZDEyLWU0NTYtNGEwNS05NzNlLTgyZDcwODEyYTYwYiIsImlhdCI6MTU5MjQ1MDcyNCwic3ViIjoiZGV2ZWxvcGVyLzQ1ODU4MmVjLTNhNDgtZDUzNC1kNWZmLTdiYjBkMmI5ZTU2MiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxNzcuNDUuMzUuMjUiXSwidHlwZSI6ImNsaWVudCJ9XX0.e5FZ0AzCrW_VYVwI5D1PJCSAR6nJWU8YOdDMaL71TnJU6NkJ-JwILuSB7P5jximgxWv1nIaxi6IMgLU0weDu9w'
+        }
+        
+        # Efetuando a segunda requisição a API
+        response_clans_members = requests.get(url_clans_members, headers=headers, data = payload)
+        
+        # Convertendo retorno para texto
+        data_clans_members = response_clans_members.text.encode('utf8')
+        
+        # Convertendo retorno para JSON
+        j_data_clans_members = json.loads(data_clans_members)
+        items_clans_members = j_data_clans_members['items']
+        
+        # Criando nova lista
+        member_list = []
+        
+        # Percorrendo a lista do retorno para pegar apenas as informações necessárias
+        for item_member in items_clans_members:
+            member = {}
+            if 'name' in item_member and item_member['name']:
+                member['nome'] = item_member['name']
+            if 'expLevel' in item_member and item_member['expLevel']:
+                member['level'] = item_member['expLevel']
+            if 'trophies' in item_member and item_member['trophies']:
+                member['troféus'] = item_member['trophies']
+            if 'role' in item_member and item_member['role']:
+                member['papel'] = item_member['role']
+            
+            member_list.append(member)
+            
+            # Retorna lista de informações dos membros do clan
+            print(member_list)
+            logger.console(member_list)
+            
+            return member_list
+            
+        logger.console("ENTROU NO SALVA CSV")
+        logging.info('Gerando arquivo member_list.csv')
+        
+        # Convertendo a lista de informações de membros do clan em dataframe
+        df = pd.DataFrame(member_list)
+        
+        # Salvando dataframe em formato CSV
+        df.to_csv('member_list.csv', index=None, encoding='utf-8-sig')
+        
+        # Retorno 
+        print("Sucesso!")
+        logger.console("Sucesso!")
+        
+        return {}
+        
+    except Exception as ex:
+        logging.error(ex)
+        raise ex
--- teste_case.robot
+++ teste_case.robot
@@ -1,6 +1,10 @@
 *** Settings ***
 Library           SeleniumLibrary
-Variables         ptest.py
+Library           OperatingSystem
+Library           BuiltIn
+Library           Collections
+Library           Process
+Library           clash.py
 
 *** Variables ***
 ${URL}          https://developer.clashroyale.com/#/login
@@ -9,44 +13,78 @@ ${email}        lucasnaldo@gmail.com
 ${password}     123lu123
 ${key}          teste
 ${description}     testeDescription
-${var}=         ${varip}     
+${path}         
+${tokenstr}
+${getc}
+${getm}
+${save}        
+
 
 
 *** Test Cases ***
 Log in
     Open Browser    ${URL}    ${BROWSER}
     Log in    ${email}    ${password}
+    Menu Superior
+    New Key
+    Proc
+
 
 *** Keywords ***
 My Keyword
     [Arguments]    ${path}
     Directory Should Exist    ${path}
-    LOG                       ${path}
+    LOG TO CONSOLE            ${path}
+    Set Log Level    DEBUG
 
 Open ClashRoyale by Chrome
+    # Abre o chromedriver
     Open Browser    ${URL}    ${BROWSER}
-    LOG                       ${URL}
-    LOG                       ${BROWSER}
-    Wait Until Page Contains    Log In
+    LOG TO CONSOLE            ${URL}
 
 Log in
     [Arguments]    ${email}    ${password}
     Wait Until Page Contains    Log In
     Wait Until Page Contains    Email
+    # Maximiza o browser
     Maximize Browser Window 
+    # Entra com email e senha
     Input Text     email     ${email}
     Wait Until Page Contains    Password
     Input Password      password     ${password}
+    #Clica botão de login
     Click Button    Log In
     Wait Until Page Contains    Enter the Arena with Clash Royale API
+
+Menu Superior
+    # Clica no Menu superior direito
     Click Element         xpath=//*[@id="content"]/div[1]/div[2]/div[1]/header[1]/div[1]/div[1]/div[3]/div[1]/div[1]/button[1]/span[1]
+    # Clica no item My Account
     Click Element         xpath=//*[@id="content"]/div[1]/div[2]/div[1]/header[1]/div[1]/div[1]/div[3]/div[1]/div[1]/ul[1]/li[1]/a[1]
     Wait Until Page Contains    Create New Key
-    Click Element         xpath=//*[@id="content"]/div[1]/div[2]/div[1]/div[1]/section[2]/div[1]/div[1]/div[2]/ul[1]/li[1]/a[1]/div[1]
-    ${token} = Get Text   xpath=//*[@id="content"]/div[1]/div[2]/div[1]/div[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[1]/div[1]/samp[1]
-    # Click Element         xpath=//*[@id="content"]/div[1]/div[2]/div[1]/div[1]/section[2]/div[1]/div[1]/div[2]/p[1]/a[1]/span[2]
-    # Wait Until Page Contains    Create a Key
-    # Input Text     xpath=//*[@id="name"]     ${key}
-    # Input Text     xpath=//*[@id="description"]     ${description}
-    # Input Text     xpath=//*[@id="range-0"]     ${var}
-    # Click Button    Create Key
+    # Clica no botão Create New Key
+    Click Element         xpath=//*[@id="content"]/div[1]/div[2]/div[1]/div[1]/section[2]/div[1]/div[1]/div[2]/p[1]/a[1]/span[2]    
+    Wait Until Page Contains    Create a Key
+
+New Key
+    # Digita Key Name, Description
+    Input Text     xpath=//*[@id="name"]     ${key}
+    Input Text     xpath=//*[@id="description"]     ${description}
+    # Traz ip por chamada python a API
+    ${var}=  evaluate  clash.ip_get()  modules=clash
+    # Cola IP no campo
+    Input Text     xpath=//*[@id="range-0"]     ${var}
+    # Clica no botão Create Key
+    Click Button    Create Key
+    Wait Until Page Contains    teste
+    # Clica no Key Name desejado
+    Click Element         xpath=//*[@id="content"]/div[1]/div[2]/div[1]/div[1]/section[2]/div[1]/div[1]/div[2]/ul[1]/li[1]/a[1]/div[1]/h4[1]
+    Wait Until Page Contains    Back to My Keys
+    # Copia o Token
+    ${token}=   Get Text   xpath=//*[@id="content"]/div[1]/div[2]/div[1]/div[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[1]/div[1]/samp[1]
+    ${tokenstr}=    Convert To String   ${token}
+    Log To Console      ${tokenstr}
+
+Proc
+    ${getc}=   Start Process           python   clash.get_clan(${tokenstr})
+    Log To Console      ${getc}
